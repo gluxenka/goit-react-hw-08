2b9847eeec4d6f6ca93b1e45219ce6a92e92ce88
Kateryna Hlukhenka <gluxenka750@gmail.com>
2025-01-31 14:32:30 +0100

    add contacts state clean on logout

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 import { addContact, deleteContact, fetchContacts } from "./operations";
+import { logout } from "../auth/operations.js";
 
 const handlePending = (state) => {
   state.loading = true;
@@ -37,7 +38,10 @@ const slice = createSlice({
         state.loading = false;
         state.items = state.items.filter(({ id }) => id !== action.payload.id);
       })
-      .addCase(deleteContact.rejected, handleRejected);
+      .addCase(deleteContact.rejected, handleRejected)
+      .addCase(logout.fulfilled, (state) => {
+        state.items = [];
+      });
   },
 });
 
